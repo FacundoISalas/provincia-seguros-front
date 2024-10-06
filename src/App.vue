aae2cabe6a632fb25e56401939dc171a71d1787b
FacundoISalas <facundosalasu19@gmail.com>
2024-10-05 22:03:03 -0300

    fix navbar

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <v-app>
+    <v-layout>
     <v-main>
       <v-app-bar
         v-if="userDataStore.getIsUserAuth"
         elevation="2" 
       >
       <template v-slot:prepend>
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon
+        v-if="$vuetify.display.mobile"
+        @click.stop="drawer = !drawer"
+        >
+        </v-app-bar-nav-icon>
       </template>
       <v-app-bar-title>
       <router-link to="/">
@@ -16,6 +21,7 @@
         width="160"
         height="160"
         aspect-ratio="16/9"
+        v-tooltip:start="$t('globalHome')"
         >
         </v-img>
       </router-link>
@@ -42,7 +48,7 @@
         v-model="isUserMenuOn"
       >
         <template v-slot:activator="{ props }">
-          <div class="d-flex justify-space-between align-center mx-2 px-1">
+          <div v-tooltip:bottom="$t('globalOptions')" class="d-flex justify-space-between align-center mx-2 px-1">
             <v-btn
               color="secondary"
               v-bind="props"
@@ -55,16 +61,60 @@
             </p>
           </div>
         </template>
+        <v-card>
+          <v-card-text>
+            <v-btn
+              fab
+              v-tooltip:bottom="$t('globalLogout')"
+              small
+            >
+              {{ $t('globalLogout') }}
+              <v-icon
+                color="secondary"
+                class="ml-1"
+                small
+              >
+                mdi-logout
+              </v-icon>
+            </v-btn>
+          </v-card-text>
+        </v-card>
       </v-menu>
     </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      :location="$vuetify.display.mobile ? 'bottom' : undefined"
+      temporary
+    >
+      <v-list>
+        <v-list-item
+        :to="'/'"
+        append-icon="mdi-home"
+        base-color="secondary"
+        >
+          <v-list-item-title> <span style="color: black;">{{ $t('globalHome') }}</span></v-list-item-title>
+        </v-list-item>
+        <v-list-item v-for="item in menuItems"
+        :key="item.title"
+        :to="item.path ? item.path : ''"
+        :append-icon="item.icon"
+        base-color="secondary"
+        >
+          <v-list-item-title> <span style="color: black;">{{ item.title }}</span></v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
       <router-view />
     </v-main>
+    </v-layout>
   </v-app>
 </template>
 
 <script lang="ts" setup>
 import { ref } from 'vue';
 import { useUserData } from '@/store/userDataStore';
+import { useI18n } from 'vue-i18n';
+const { t } = useI18n();
 
 // interfaces
 interface menuItemsInterface {
@@ -77,27 +127,29 @@ const userDataStore = useUserData();
 // data
 const userName = ref<string>(userDataStore.getUserName);
 const isUserMenuOn = ref<boolean|undefined>(false);
+const drawer = ref<boolean|undefined>(false);
+
 const menuItems = ref<menuItemsInterface[]>([
   {
-    title: 'Seguros personas',
+    title: t('globalPersonalInsurance'),
   },
   {
-    title: 'Seguros Empresas',
+    title: t('globalCompanyInsurance'),
   },
   {
-    title: 'Clientas/es',
+    title: t('globalCustomers'),
   },
   {
-    title: 'PAS',
+    title: t('globalPas'),
   },
   {
-    title: 'Proveedoreas/es',
+    title: t('globalProviders'),
   },
   {
-    title: 'Quiénes somos',
+    title: t('globalAboutUs'),
   },
   {
-    title: 'Sucursales',
+    title: t('globalBranches'),
     path: '/sucursales',
     icon: 'mdi-bank-circle-outline'
   },
